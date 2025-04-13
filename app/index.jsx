fd6464621755e09c88b71e2a6dbb80468facadd8
Liam <lkhant616@gmail.com>
2025-04-13 16:10:09 +0630

    Lesson 3 & 4 completed

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View, Image } from "react-native";
+import { Link } from "expo-router";
 import Logo from "../assets/img/logo_light.png";
 import React from "react";
 
@@ -8,6 +9,12 @@ const Home = () => {
       <Image source={Logo} style={styles.img} />
       <Text style={[styles.title]}>The Number 1</Text>
       <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading list app</Text>
+      <Link href="/about" style={styles.link}>
+        About Page
+      </Link>
+      <Link href="/contact" style={styles.link}>
+        Contact Page
+      </Link>
     </View>
   );
 };
@@ -28,5 +35,10 @@ const styles = StyleSheet.create({
 
   img: {
     marginVertical: 20,
+  },
+
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1,
   },
 });
